18f78d2b51f6654cc7b96d7117a7e35739ccb5ad
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-20 18:36:34 +0300

    Added twitter bootstrap and registred in the main clas

--- app/main/views.py
+++ app/main/views.py
@@ -3,5 +3,5 @@ from . import main
 @main.route("/")
 def index():
     title="Pitch"
-    message="Home of ideas"
+    message="Home of ideas, Where ideas are born"
     return render_template("index.html",title=title,message=message)
