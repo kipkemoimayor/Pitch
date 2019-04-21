c2f80ebc2356f239a34818eabf4e21d9503802fd
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-21 17:30:38 +0300

    Created a profile file

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,32 @@ def index():
     pitch=Pitches.query.all();
 
     return render_template("index.html",title=title,message=message,pitch=pitch)
+
+
+@main.route("/user/<uname>")
+def profile(uname):
+    user=User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html",user=user)
+
+'''
+new Pitch idea
+'''
+
+@main.route("/pitch/<uname>")
+@login_required
+def new_pitch(uname):
+
+    user=User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template("new_pitch/pitch.html")
+
+
+@main.route("/review/<id>")
+def review(id):
+
+    return render_template("/review/review.html")
