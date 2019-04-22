29857925c5261bbe9fe3e0cc70687e86ef27cc72
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-22 11:54:39 +0300

    This is to add a markdown module

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://collo:collins@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY="colo"
+
+    #markdown configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 class DevConfig(Config):
     DEBUG=True
 
