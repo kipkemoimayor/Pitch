000ce9263d1f176610e4092e08c9eadcd2b2bda3
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-23 15:48:25 +0300

    Deployment

--- config.py
+++ config.py
@@ -11,11 +11,12 @@ class Config:
     mail configurations
     '''
 
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://collo:collins@localhost/pitch_test'
 class DevConfig(Config):
